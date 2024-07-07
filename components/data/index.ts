b0a14ef05cd4b0690d2b35655a15f738e5b8ea44
Bernard <bernardacero@rocketmail.com>
2024-07-07 14:41:17 +0800

    update index.ts images link

--- components/data/index.ts
+++ components/data/index.ts
@@ -14,7 +14,7 @@ export const gridItems = [
     className: "lg:col-span-5 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "./b1.svg",
     spareImg: "",
   },
   {
@@ -44,8 +44,8 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
+    img: "./grid.svg",
+    spareImg: "./b4.svg",
   },
 
   {
@@ -55,8 +55,8 @@ export const gridItems = [
     className: "md:col-span-5 md:row-span-3",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
+    img: "./b5.svg",
+    spareImg: "./grid.svg",
   },
   {
     id: 6,
@@ -75,24 +75,24 @@ export const projects = [
     id: 1,
     title: "Nike Website",
     des: "Recreated Nike Website using ReactJS and Tailwind.",
-    img: "/nike.svg",
-    iconLists: ["/re.svg", "/tail.svg",],
+    img: "./nike.svg",
+    iconLists: ["./re.svg", "./tail.svg",],
     link: "https://github.com/bernard-lab/Nike-Website.git",
   },
   {
     id: 2,
     title: "Image Slider",
     des: "Created a smooth image transition animation of wildlife animals combining CSS and JavaScript.",
-    img: "/img-slider.png",
-    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    img: "./img-slider.png",
+    iconLists: ["./html.svg", "./css.svg", "./js.svg"],
     link: "https://github.com/bernard-lab/image-slider.git",
   },
   {    
     id: 3,
     title: "Animated Apple Iphone 3D Website",
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects.",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    img: "./p4.svg",
+    iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./three.svg", "./gsap.svg"],
     link: "https://github.com/bernard-lab/apple-website.git",
   },
  
@@ -100,8 +100,8 @@ export const projects = [
     id: 4,
     title: "YouTube Website",
     des: "Recreated the old YouTube Website, using HTML, CSS, and JavaScript.",
-    img: "yt.png",
-    iconLists: ["/html.svg", "/css.svg", "/js.svg"],
+    img: "./yt.png",
+    iconLists: ["./html.svg", "./css.svg", "./js.svg"],
     link: "https://github.com/bernard-lab/yt-recreate.git",
   },
   
@@ -113,45 +113,45 @@ export const workExperience = [
     title: "Chat Support Representative",
     desc: "Ensures prompt and proper resolution of customer queries by providing answers, additional information, suggestions to provide a solution according to defined procedures.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "./exp1.svg",
   },
   {
     id: 2,
     title: "Technical Support Representative",
     desc: "Identifies, investigates, and resolves users’ problems with computer software and hardware, or escalating/engaging others in the organization to provide a solution according to defined procedures.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    thumbnail: "./exp2.svg",
   },
   {
     id: 3,
     title: "TECHNICAL OFFICER",
     desc: "Responsible for repairing and maintaining computers and servers including hardware, software, network, and computer peripherals.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    thumbnail: "./exp3.svg",
   },
   {
     id: 4,
     title: "Software Developer Intern",
     desc: "Developed user-facing features using Java NetBeans.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "./exp4.svg",
   },
 ];
 
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "./git.svg",
     link: 'https://github.com/bernard-lab'
   },
   {
     id: 2,
-    img: "/link.svg",
+    img: "./link.svg",
     link: "https://www.linkedin.com/in/bernardacero/"
   },
   {
     id: 3,
-    img: "/fb.svg",
+    img: "./fb.svg",
     link: "https://www.facebook.com/pushstart.bern/"
   },
 ];
