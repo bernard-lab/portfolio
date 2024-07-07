729fe7dccebcd9fd72a2786af496f2990a1b8761
Bernard <bernardacero@rocketmail.com>
2024-07-07 14:34:26 +0800

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
                 {socialMedia.map((profile) => (
                     <a href={profile.link} target='_blank' key={profile.id}
                     className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-                        <img src={profile.img} alt={profile.id} width={20} height={20}
+                        <img src={profile.img} alt='profile image' width={20} height={20}
                         
                     />
                     </a>
