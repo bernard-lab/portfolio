612c51460afba459dd638f7f453e76b710ac36eb
Bernard <bernardacero@rocketmail.com>
2024-07-07 15:02:50 +0800

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Modern & Creative Portfolio",
   description: "Modern & Minimalist Portfolio",
   authors: [{name:'Bernard Acero', url:'https://github.com/bernard-lab'}],
   creator: 'Bernard Acero',
