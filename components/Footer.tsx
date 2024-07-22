35889527a4e8302fba76c7ffd2d86f7ab35d6797
Bernard <bernardacero@rocketmail.com>
2024-07-22 18:41:16 +0800

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,11 +31,11 @@ const Footer = () => {
        </div>
 
         {/* Copyright and Socials */}
-       <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
+       <div className='flex mt-16 md:flex-row flex-col justify-between items-center space-y-3'>
             <p className='md:textbase text-sm md:font-normal font-light'>
                 Copyright &copy; 2024 Bernard
             </p>
-            <div className='flex items-center md:gap-3 gap-6`'>
+            <div className='flex items-center gap-3 '>
                 {socialMedia.map((profile) => (
                     <a href={profile.link} target='_blank' key={profile.id}
                     className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-white-300 hover:scale-125 transition-transform'>
